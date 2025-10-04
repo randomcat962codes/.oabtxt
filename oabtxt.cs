5493babeec1e531976d6630d7552b6170a1edbc0
Ovadiah <landonp1287913+github@gmail.com>
2025-10-04 09:33:00 -0500

    Created the CharEncoding class and added the dictionary for encoded charcters.

--- oabtxt.cs
+++ oabtxt.cs
@@ -1,3 +1,15 @@
+public class CharEncoding //Contains the data and functions for encoding text
+{
+    Dictionary<char, char[]> encodings = new() //Contains the encoded text
+    {
+        {'C', ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']},
+        {'L', ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']},
+        {'N', ['1','2','3','4','5','6','7','8','9','0']},
+        {'P', ['.',',','!','?']},
+        {'S', ['~','`','@','#','$','%','^','&','*','(',')','_','-','+','=','|','\\','{','}','[',']',':',';','"','\'','<','>','/']}
+    };
+}
+
 class Program
 {
     public static void Main(string[] args)
